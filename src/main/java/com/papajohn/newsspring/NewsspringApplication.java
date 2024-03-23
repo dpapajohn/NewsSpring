4dc203964137183851714119422ae3b5d73b49dc
Daniel J. Papajohn <dpapajohn@gmail.com>
2024-03-22 20:35:47 -0700

    Initial creation of Spring Boot project with maven, via the pring boot initializer

---
+++ src/main/java/com/papajohn/newsspring/NewsspringApplication.java
@@ -0,0 +1,13 @@
+package com.papajohn.newsspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/papajohn/newsspring/NewsspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.papajohn.newsspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
